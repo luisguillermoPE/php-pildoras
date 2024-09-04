80ebcf37158285ba02d0721000e98d15d59e132b
Luis Guillermo <luis.garro.pe@gmail.com>
2024-09-04 00:40:54 -0500

    segundo

--- 31-arrays.php
+++ 31-arrays.php
@@ -3,20 +3,39 @@
 
 $users = array("name"=>"Steve","lastname"=>"Tyler","job"=>"singer");
 $rooms = ["one"=>"perú","two"=>"ecuador"];
-$pass = "124";
+$pass = array(1,2,3,4,5);
 
-echo $rooms["one"];
+// echo $rooms["one"];
 // echo $users["name"];
 
 echo "\n";
 
-if (is_array($pass)){
-    echo "es un array";
-}
-else
+// if (is_array($pass)){
+//     echo "es un array";
+// }
+// else
+// {
+//     echo "no es un array";
+// };
+
+
+foreach($rooms as $i=>$value)
 {
-    echo "no es un array";
+    // echo $i." le corresponde el país: ".$value."\n";
+    echo "$i. le corresponde el país: $value \n";
 };
+
+for ($i=0;$i<count($pass);$i++){
+    echo "$pass[$i] \t";
+};
+
+echo "\n";
+// echo $rooms["one"];
+
+for ($a=0;$a<count($rooms);$a++){
+    echo "$a \t";
+};
+
 
 
 ?>
