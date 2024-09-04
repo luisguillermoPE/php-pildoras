9e0bcbccc43568055f9bd8f9f57822a9e6b3096d
luisguillermoPE <luis.garro.pe@gmail.com>
2024-09-03 22:01:07 -0500

    first commit

--- 30-arrays.php
+++ 30-arrays.php
@@ -11,14 +11,31 @@ $week[]="thursday";
 
 // echo $week[1];
 
-foreach($week as $i => $w){
-    echo $i."-> ".$w."\n";
-}
+// foreach($week as $i => $w){
+//     echo $i."-> ".$w."\n";
+// }
 
 
 $array = array(1, 2, 3, 4);
 foreach ($array as $valor) {
     $arrayy[] = $valor * 2;
 }
-var_dump($arrayy);
+// var_dump($arrayy);
+
+
+$users = array("name"=>"Steve","lastname"=>"Tyler","job"=>"singer");
+$pass = "124";
+
+// echo $users["name"];
+
+
+if (is_array($pass)){
+    echo "es un array";
+}
+else
+{
+    echo "no es un array";
+};
+
+
 ?>
